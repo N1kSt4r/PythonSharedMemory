1b12a58213ff2d95370455305f5020e3d0c3ee49
n1kst4r <nikita2000tsuranov@gmail.com>
2021-11-16 17:18:10 +0300

    base sync

--- SharedStore.h
+++ SharedStore.h
@@ -3,18 +3,30 @@
 #include <boost/python/tuple.hpp>
 #include <boost/python/numpy.hpp>
 
+#include <boost/interprocess/sync/named_mutex.hpp>
+#include <boost/interprocess/sync/scoped_lock.hpp>
+#include <boost/interprocess/sync/interprocess_mutex.hpp>
+#include <boost/interprocess/sync/interprocess_semaphore.hpp>
+
 #include <boost/interprocess/containers/map.hpp>
 #include <boost/interprocess/allocators/allocator.hpp>
 #include <boost/interprocess/managed_shared_memory.hpp>
 
+#include <chrono>
+#include <vector>
 #include <string>
-#include <array>
+
+#include <iostream>
+
 
 #define MB *1014*1024
 
 namespace py = boost::python;
 namespace np = boost::python::numpy;
 namespace sh = boost::interprocess;
+
+sh::named_mutex mutex(sh::open_or_create, "Check");
+sh::named_mutex init_store_lock(sh::open_or_create, "init_store_lock");
 
 
 template<class T>
@@ -43,7 +55,9 @@ public:
     ~SharedStore();
 
     void finalize();
+    double get_time();
     size_t key2int(const PyKeyType&);
+//    void active_wait(const PyKeyType&);
 
     // ToDo: use named mutex for sync and write method with dict/list
     void insert(const PyKeyType& key, const PyValueType& value);
@@ -53,12 +67,19 @@ public:
     void get_dict(py::dict& dict);
 
 private:
+//    sh::offset_ptr<sh::interprocess_semaphore> _reader;
+//    sh::offset_ptr<sh::interprocess_mutex> _writer;
+
     sh::managed_shared_memory _segment;
+    sh::offset_ptr<double> _timestamp;
     sh::offset_ptr<StoreType> _store;
     std::hash<std::string> _hasher;
     static bool _inited;
     std::string _name;
     bool _is_server;
+
+//    int _active_wait_attempts;
+//    int _active_wait_secs;
 };
 
 
@@ -76,21 +97,24 @@ void from_ndarray(const np::ndarray& ndarray, std::vector<T, A>* vector) {
 
 template<class T>
 SharedStore<T>::SharedStore(const char* name, size_t size, bool is_server) {
+    sh::scoped_lock<sh::named_mutex> lock(init_store_lock);
+
     _is_server = is_server;
     assert(not _inited);
     _inited = true;
-
-    std::string _name(name);
-    std::string store_name = _name + "_store";
+    _name = name;
 
     if (is_server) {
         sh::shared_memory_object::remove(name);
         _segment = sh::managed_shared_memory(sh::create_only, name, size MB);
         ShMapAllocType sh_allocator(_segment.get_segment_manager());
-        _store = _segment.construct<StoreType>(store_name.c_str())(std::less<>(), sh_allocator);
+
+        _store = _segment.construct<StoreType>("store")(std::less<>(), sh_allocator);
+        _timestamp = _segment.construct<double>("timestamp")(get_time());
     } else {
         _segment = sh::managed_shared_memory(sh::open_or_create, name, size MB);
-        _store = _segment.find<StoreType>(store_name.c_str()).first;
+        _timestamp = _segment.find<double>("timestamp").first;
+        _store = _segment.find<StoreType>("store").first;
     }
 }
 
@@ -109,6 +133,24 @@ void SharedStore<T>::finalize() {
 template<class T>
 size_t SharedStore<T>::key2int(const PyKeyType& key) {
     return _hasher(py::extract<std::string>(key));
+}
+
+//template<class T>
+//void SharedStore<T>::active_wait(const PyKeyType& key) {
+//    auto c_key = key2int(key);
+//    for (int i = 0; i < _active_wait_attempts; ++i) {
+//        if (_store->count(c_key)) {
+//            return;
+//        }
+//        sleep(_active_wait_secs);
+//    }
+//    throw std::runtime_error("Key not found");
+//}
+
+template<class T>
+double SharedStore<T>::get_time() {
+    return std::chrono::duration_cast<std::chrono::duration<double>>(
+            std::chrono::system_clock::now().time_since_epoch()).count();
 }
 
 template<class T>
@@ -133,6 +175,9 @@ typename SharedStore<T>::PyValueType SharedStore<T>::get(
 
 template<class T>
 void SharedStore<T>::insert_dict(const py::dict& dict) {
+    sh::scoped_lock<sh::named_mutex> lock(mutex);
+
+    std::cout << "insert start" << std::endl;
     auto keys = dict.keys();
     auto values = dict.values();
     for (size_t i = 0; i < len(keys); ++i) {
@@ -140,10 +185,17 @@ void SharedStore<T>::insert_dict(const py::dict& dict) {
          np::ndarray value = py::extract<np::ndarray>(values[i]);
          insert(key, value);
     }
+    std::cout << "insert end" << std::endl;
+
+    *_timestamp = get_time();
+    printf("set %lf %lf\n", *_timestamp, get_time());
 }
 
 template<class T>
 void SharedStore<T>::get_dict(py::dict& dict) {
+    sh::scoped_lock<sh::named_mutex> lock(mutex);
+
+    std::cout << "get start" << std::endl;
     auto keys = dict.keys();
     auto values = dict.values();
     for (size_t i = 0; i < len(keys); ++i) {
@@ -151,6 +203,10 @@ void SharedStore<T>::get_dict(py::dict& dict) {
         np::ndarray value = py::extract<np::ndarray>(values[i]);
         get(key, value);
     }
+    std::cout << "get end" << std::endl;
+
+    std::cout << get_time() - *_timestamp << std::endl;
+    printf("get %lf %lf\n", *_timestamp, get_time());
 }
 
 template<class T> bool SharedStore<T>::_inited = false;
