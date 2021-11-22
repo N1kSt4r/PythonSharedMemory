32dc443614ad2b399c1a8a353243f5a20717b0c2
n1kst4r <nikita2000tsuranov@gmail.com>
2021-11-22 22:37:19 +0300

    add scripts to test values from store

---
+++ cv_reader.py
@@ -0,0 +1,13 @@
+from shared_tools import SharedStore_uint8
+import numpy as np
+import cv2
+
+data = {"check": np.full((416, 416, 3), 120, dtype=np.uint8)}
+
+store = SharedStore_uint8("check", 400, False)
+for i in range(10000):
+    store.get_dict(data)
+    cv2.imshow("check", data["check"])
+    cv2.waitKey(150)
+
+store.finalize()
---
+++ cv_writer.py
@@ -0,0 +1,11 @@
+from shared_tools import SharedStore_uint8
+from time import sleep
+import numpy as np
+
+store = SharedStore_uint8("check", 400, True)
+for i in range(100000):
+    store.insert_dict({"check": np.full((416, 416, 3), 0 if i % 2 == 0 else 255, dtype=np.uint8)})
+    print(0 if i % 2 == 0 else 255)
+    sleep(1/30)
+    
+store.finalize()
